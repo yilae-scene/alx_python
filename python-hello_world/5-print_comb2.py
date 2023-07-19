a6a7bba1a15ee192cd9328a792011b39ce5458fd
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-19 18:15:00 +0300

    Add a python file that prints double digit

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -2,6 +2,6 @@
 #have a range between (0, 100)
 for i in range(0 , 100):
     if (i < 10):
-        print("0{}".format(i), end =",")
+        print("0{}".format(i), end =", ")
     else:
-        print("{}".format(i), end =",")
+        print("{}".format(i), end =", ")
