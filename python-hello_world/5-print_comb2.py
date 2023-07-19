bb49b41be6ad71534c01568cdfa0db4adf11cfa9
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-19 19:20:12 +0300

    Add comment on file

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,7 +1,7 @@
 #have a range between (0, 100)
 
 for i in range(0 , 100):
-    if (i < 99 ):  # print until it reaches 99 so to remove comma in the end
+    if (i < 99 ):  
         print("{:02d}".format(i), end =", ")
     else:
         print("{}".format(i))
