f219686698bfe1aa40e0694cf2cb304425dff3e9
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-19 18:10:24 +0300

    Add a python that pritns double digit

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -3,11 +3,7 @@
 for i in range(0 , 100):
    
 #numbers printed should be in two digits. 
-
-#for (0 to 9 ) we can add 0 infront
     if (i < 10):
         print("0{}".format(i), end =",")
-
-#for rest just print
     else:
         print("{}".format(i), end =",")
