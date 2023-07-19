989778298f12191de1b4bd35468ee1e7bf91240d
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-19 18:36:08 +0300

    File that prints double digit ints.

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,7 +1,7 @@
 
 #have a range between (0, 100)
 for i in range(0 , 100):
-    if (i < 10):
-        print("0{}".format(i), end =", ")
+    if (i < 99 ):  # print until it reaches 99 so to remove comma in the end
+        print("{:02d}".format(i), end =", ")
     else:
-        print("{}".format(i), end =", ")
+        print("{}".format(i))
