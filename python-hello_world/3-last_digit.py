cb1c57b7c52b360f78b377eae8566d89ce084578
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-19 12:11:46 +0300

    Add another py.file

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,8 +3,6 @@ import random
 number = random.randint(-10000, 10000)
 number1= int(str(number)[-1])
 
-if (number < 0):
-     number1=-(number1)
 if (number1 > 5):
     print("Last digit of {} is {} and is greater than 5".format(number,number1))
 elif (number1 < 6 and number1 > 0):
