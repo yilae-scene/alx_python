2972be9eda9fb7d836dfc795f740063461eef29d
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-20 13:16:12 +0300

    Modify the function.

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -2,6 +2,9 @@
 # convert from fahrenheit to celsius
 
 def convert_to_celsius(fahrenheit):
-    celsius = 5/9 * (fahrenheit - 32)
+    celsius = (5/9 * (fahrenheit - 32))
+    #ternicate so that it prints only two decimal places.
+    celsius = ("%0.2f" %celsius)
     return celsius
 
+print(convert_to_celsius(-459.67))
