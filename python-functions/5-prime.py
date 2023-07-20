fae7dd2eadb27f62e2772fbd7333e738d49c07a4
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-20 17:42:55 +0300

    Add prime.py file

---
+++ python-functions/5-prime.py
@@ -0,0 +1,15 @@
+
+# a function that checks a number is prime or not. 
+
+def is_prime(number):
+    count = 0
+    # itereat through to find it is evenly divided morethan twice
+    for i in range (1 , number + 1):
+        if (number % i == 0):
+            count+=1
+    if count == 2 or number == 2:
+        return True
+    else:
+        return False
+
+print(is_prime(0))
