626963fe2587fc51d5a8475e57579ecdbed3e476
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-20 20:00:03 +0300

    Modify last_digit with abs

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,7 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    last_digit = number % -10
+    last_digit = abs(number) % 10
+    last_digit = - last_digit
+
     if last_digit == 0:
         print("Last digit of {} is {} and is 0".format(number,last_digit))
     else:
