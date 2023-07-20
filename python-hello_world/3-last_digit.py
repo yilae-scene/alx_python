3c03096b8ed6ea6a7e6e83b4fdf65ff19d837b16
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-20 12:15:04 +0300

    Last Modification on 3-last.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,19 +2,19 @@ import random
 
 number = random.randint(-10000, 10000)
 
-if (number < 0):
+if number < 0:
     last_digit = number % -10
-    if (last_digit == 0):
+    if last_digit == 0:
         print("Last digit of {} is {} and is 0".format(number, last_digit))
     else:
         print(
             "Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
-elif (number > 0):
+elif number > 0:
     last_digit = number % 10
-    if (last_digit > 5):
+    if last_digit > 5:
         print("Last digit of {} is {} and is greater than 5".format(
             number, last_digit))
-    elif (last_digit < 6 and last_digit > 0):
+    elif last_digit < 6 and last_digit > 0:
         print("Last digit of {} is {} and is less than 6 and not 0".format(
             number, last_digit))
     else:
