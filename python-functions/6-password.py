0bb9b543ab608ab1ef0796cab129daca31412763
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-21 11:54:29 +0300

    Add a password checker.py

---
+++ python-functions/6-password.py
@@ -0,0 +1,13 @@
+
+# here is a function that validates password.
+def validate_password(password):
+    #have the length of a string in variable
+    length = len(password)
+    # Have the conditions specified
+    if (password.isupper() == False) == (password.islower() == False) and (length >= 8) and not(" " in password):
+        print (True)
+    else:
+        print(False)
+ 
+
+
