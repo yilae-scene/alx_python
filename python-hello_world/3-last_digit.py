f2687f991f1bd735f0c85b42af0f561fa68f1bfb
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-21 20:43:27 +0300

    Modift print statement

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -10,8 +10,7 @@ if number < 0:
     if last_digit == 0:
         print("Last digit of",number,"is",last_digit,"and is 0")
     else:
-        print(
-            "Last digit of",number,"is",last_digit,"and is less than 6 and not 0")
+        print("Last digit of",number,"is",last_digit,"and is less than 6 and not 0")
 # if number is more than
 elif number > 0:
     last_digit = number % 10
