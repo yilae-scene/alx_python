3218fe8c8ce9d7065510b4462c5b1f12f3953b64
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-21 17:42:18 +0300

    Modified to include numbers.

--- python-functions/6-password.py
+++ python-functions/6-password.py
@@ -4,10 +4,10 @@ def validate_password(password):
     #have the length of a string in variable
     length = len(password)
     # Have the conditions specified
-    if (password.isupper() == False) == (password.islower() == False) and (length >= 8) and not(" " in password):
+    if (password.isupper() == False) == (password.islower() == False) and (length >= 8) and not(" " in password) and not(password.isalpha()):
         print (True)
     else:
         print(False)
  
-
+validate_password("wllllllllllllllK2")
 
