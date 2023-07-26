182d7dd4f7d7046640b2cfb5e83cc3d36506c02b
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-26 11:30:14 +0300

    improve CLI argument

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -9,6 +9,9 @@ if __name__ == '__main__':
     n = len(sys.argv)
     if n == 1:
         print("0 arguments.")
+    elif n==2:
+        print("1 argument:".format(n-1))
+        print("1: "+sys.argv[1])
     else:
         print("{} arguments:".format(n-1))
         for x in range(1, n):
