b56699f80228374ca1c6999dec896e588376ec38
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-26 11:22:54 +0300

    Modify CLI input

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     n = len(sys.argv)
     if n == 1:
         print("0 arguments.")
-    n = len (sys.argv)
-    for x in range(1,n):
-        print("{}: {}".format(x,sys.argv[x]))
-
+    else:
+        print("{} arguments :".format(n-1))
+        for x in range(1, n):
+            print("{}: {}".format(x, sys.argv[x]))
