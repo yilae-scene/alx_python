952c36d842229d799ad496b2c8b955c8a7203cb1
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-26 10:03:54 +0300

    Initail commit

---
+++ python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+
+# a function that simply adds two numbers so that they can be imported.
+
+def add (a , b):
+    """ here is a function that takes two arguments a and b
+    and 
+      returns their sum as
+      return a + b
+      """
+    return a + b
+
+
