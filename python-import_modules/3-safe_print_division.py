b9a3ecf0a8a56a4a1e040b5648a8e7670da44d88
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-26 14:52:15 +0300

    Modify

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -9,6 +9,6 @@ def safe_print_division(a , b):
         result = None
     finally:
         print("Inside result: {}".format(result))
-        print("{:d} / {:d} = {}".format(a , b, result))
+        #print("{:d} / {:d} = {}".format(a , b, result))
     return result
 
