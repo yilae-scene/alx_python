6fa5306146c34a83b53e454a27e51ad9739a4fe1
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-26 12:20:10 +0300

    Add try exception errors

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,15 @@
+# simple function that should divid a and b through
+#...try and catch exception
+
+
+
+def safe_print_divison(a , b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        print("{:d} / {:d} = {}".format(a , b, result))
+
+safe_print_divison(12 ,0)
