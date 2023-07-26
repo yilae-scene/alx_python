e0eaf8c9b378215b44abb7f7402be5128888b380
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-26 12:57:17 +0300

    Add try and catch errors

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -5,11 +5,11 @@
 def safe_print_divison(a , b):
     try:
         result = a / b
+        return result
     except ZeroDivisionError:
         result = None
+        return result
     finally:
         print("Inside result: {}".format(result))
         print("{:d} / {:d} = {}".format(a , b, result))
-
-# safe_print_divison(a=10 , b=-5)
 
