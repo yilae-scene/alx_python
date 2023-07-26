dc1ed3b855f6265f8a077d49ad6dda9cbbcd3999
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-26 14:50:06 +0300

    Modify

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -5,7 +5,7 @@
 def safe_print_division(a , b):
     try:
         result = a / b
-    except (ZeroDivisionError,TypeError):
+    except ZeroDivisionError:
         result = None
     finally:
         print("Inside result: {}".format(result))
