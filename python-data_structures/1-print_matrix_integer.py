9412f0d75bd0189782c31e669a377e79b18efff7
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-27 22:28:00 +0300

    jjj

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,18 +1,20 @@
 
 # a function that prints a matrix of integers.
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix)==0:
+        print(end = ' ')
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            if j < len(matrix[i])-1 or matrix == [[]]:
-                print("{:d}".format(matrix[i][j]), end='  ')
+            if j < len(matrix[i])-1 :
+                print("{:d}".format(matrix[i][j]), end=' ')
             else:
                 print("{:d}".format(matrix[i][j]))
 
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
 
-print_matrix_integer([[]])
+# print_matrix_integer([[]])
