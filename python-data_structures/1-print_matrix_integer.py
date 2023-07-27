cd3d15552f7de81f45dd7b87a67637ae373a48a6
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-27 20:01:18 +0300

    lskdjf

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -8,10 +8,10 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(matrix[i][j]),end=' ')
         print()
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+# matrix = [
+#     [1, 2, 3],
+#     [4, 5, 6],
+#     [7, 8, 9]
+# ]
 
-print_matrix_integer(matrix)
+# print_matrix_integer(matrix)
