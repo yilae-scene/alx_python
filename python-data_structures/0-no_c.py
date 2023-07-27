05442ede565554f31e7741a077d63e3ad8465a23
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-27 12:14:01 +0300

    Initial commit on this project

---
+++ python-data_structures/0-no_c.py
@@ -0,0 +1,12 @@
+
+# Remove all c from a string.
+
+def no_c(my_string):
+    new_string = ''
+    for ch in my_string:
+        if ch != 'c' and ch != 'C':
+            new_string += ch
+    return new_string
+
+
+print(no_c("C is fun!"))
---
+++ python-data_structures/README.md
@@ -0,0 +1,1 @@
+* here we dive into more of python data_structures 
