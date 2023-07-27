ad2f3719f9ab0aeb59169493f7012f223f3b02d9
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-27 20:30:29 +0300

    Modify Matrix.py

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -9,10 +9,3 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(matrix[i][j]))
 
 
-matrix = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
-
-print_matrix_integer(matrix)
