47fb9e77f3316e8173d167e07094f8e90bf4c377
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-28 10:11:04 +0300

    Concise the 3_last.digit

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -11,9 +11,9 @@ else:
     last_digit = number % 10
 
 if last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
+    print("Last digit of", number,"is " , last_digit,"and is greater than 5.")
 elif last_digit < 6 and not 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit)
+    print("Last digit of" ,number, "is" ,last_digit,"and is less than 6 and not 0"
           )
 elif last_digit == 0:
-    print("Last digit of {} is {} and is 0".format(number, last_digit))
+    print("Last digit of",number,"is " ,last_digitand,"is 0")
