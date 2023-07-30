29f8c1bb45fea6505f704d5ddb1fc1cb56449f9f
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-30 12:08:16 +0300

    Multiply using map()

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,5 +1,5 @@
+#multiply using map()
 def multiply_list_map(mylist=[] , num = 0):
-    my_new_list = list(map(lambda n : n * num, mylist))
-    return my_new_list
+    return list(map(lambda n : n * num, mylist))
 
 
