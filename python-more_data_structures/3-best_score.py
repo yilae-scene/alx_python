8f1bea892f82580ada4234ba88dd7eb85c8a0e77
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-07-30 11:34:50 +0300

    Update to accept an empty dict.

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -3,14 +3,17 @@
 
 def best_score(a_dictionary):
     try:
-        new_value = list(a_dictionary.values())
-        max = new_value[0]
-        for n in range(len(new_value)):
-            if max < new_value[n]:
-                max = new_value[n]
-        for k, v in a_dictionary.items():
-            if max == v:
-                return k
+        if a_dictionary == dit():
+            return None
+        else:
+            new_value = list(a_dictionary.values())
+            max = new_value[0]
+            for n in range(len(new_value)):
+                if max < new_value[n]:
+                    max = new_value[n]
+            for k, v in a_dictionary.items():
+                if max == v:
+                    return k
     except (ValueError, TypeError, AttributeError):
         return None
 
