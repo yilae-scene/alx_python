e17f7ff401ddc8f2b03de57be7e298654261afbe
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-01 15:35:43 +0300

    Add print for size = 0

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -44,9 +44,12 @@ class Square:
         return self.__size ** 2
 
     def my_print(self): 
-        for i in range(self.__size):
-            for j in range (self.__size):
-                print('#',end='')
+        if self.__size == 0:
             print()
+        else:
+            for i in range(self.__size):
+                for j in range (self.__size):
+                    print('#',end='')
+                print()
 
 
