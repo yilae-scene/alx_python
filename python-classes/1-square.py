b01df5aee1f18073d66d314828b773d59ae161a7
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-01 13:23:52 +0300

    Add validation for the size variable

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,6 @@
 ''' this is the first and most basic class
     creation that uses a private variable.
 '''
-
-
 class Square:
     ''' defining the class of square and initializing it with a variable size '''
 
@@ -10,14 +8,18 @@ class Square:
         ''' the first init function that has a single
             parameter that is also privare.
         '''
+
         if not isinstance(size, int):
+            # check the type of size
             raise TypeError ('size must be an integer')
         elif size < 0:
+            # check the value of size
             raise ValueError('size must be >= 0')
         else:
+            # if  all is correct print
             self__size = size
 
 
-my_square_4 = Square(-89)
-print(type(my_square_4))
-print(my_square_4.__dict__)
+# my_square_4 = Square(-89)
+# print(type(my_square_4))
+# print(my_square_4.__dict__)
--- python-more_data_structures/2-update_dicitionary.py
+++
@@ -1,6 +0,0 @@
-
-# use the key, value that are provided and update the dictionary
-
-def update_dictionary (a_dictionary, key , value):
-    a_dictionary[key] = value
-    return a_dictionary
