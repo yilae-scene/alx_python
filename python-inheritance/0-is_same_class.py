298733d1ec445ecedac4c4dbd3a797ba558af1d1
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-04 11:26:28 +0300

    Modify the function that checks type

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -2,12 +2,14 @@
     this is a function that returns true or false if an object is an exact instance of a class.
 '''
 
+
 def is_same_class(obj, a_class):
     ''' check if the object is the exact instance of the class
     '''
     #obj = object
     #a_class = Class
-    if type(object) == type(a_class):
+    if type(obj) == type(a_class):
+        print(type(obj))
         return True
     return False
 
