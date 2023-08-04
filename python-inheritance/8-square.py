1d747317efd24cd5b8e24df7cfbd5c9ecabb389b
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-04 20:29:05 +0300

    Modify

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -18,7 +18,7 @@ class Square(Rectangle):
 
     def __init__(self, size):
         self.__size = size
-        self.integer_validator(size, size)
+        super().integer_validator('size', size)
 
     def area(self):
         return self.__size ** 2
@@ -26,7 +26,10 @@ class Square(Rectangle):
     def __str__(self):
         return ("[Rectangle] {}/{}".format(self.__size, self.__size))
 
+s = Square(13)
 
+print(s)
+print(s.area())
 '''
 s = Square(13)
 
