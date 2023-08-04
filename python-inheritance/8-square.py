36088cbcbeaf91624dedfbb762c5fc5088e2db10
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-04 18:22:48 +0300

    Modify

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,10 +1,14 @@
 '''
 area to calculate that is sub-class of rectangle
 ''' 
+# import a file with number and put it in variable called Rectagle
+
 Rectangle =__import__('7-rectangle').Rectangle
 
 class square(Rectangle):
-    '''calculate the area from a size that is private
+    '''
+    calculate the area from a size that is private
+    with some uses of some function of the super class
     '''
     def __init__(self,size):
         self.__size = size
