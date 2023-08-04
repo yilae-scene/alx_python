7336a97027f62b679f31257d1f79f79b768474df
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-04 14:33:03 +0300

    Modify the documentation

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -3,8 +3,11 @@
 '''
 
 class BaseGeometry:
+    ''' the class raises a an exception'''
     def __init__(self):
+        #initalize 
         self.self = self
 
     def area(self):
+        # raise an error
         raise 'area() is not implemented'
