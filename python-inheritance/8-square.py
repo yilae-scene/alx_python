3fc03d6165531356d0b4113e3466040103ad82c6
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-04 18:44:16 +0300

    add

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -26,7 +26,7 @@ class Square(Rectangle):
         return ("[Rectangle] {}/{}".format(self.__size, self.__size))
 
 
-s = Square(13)
+# s = Square(13)
 
-print(s)
-print(s.area())
+# print(s)
+# print(s.area())
