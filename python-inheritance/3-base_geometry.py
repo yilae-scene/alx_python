8214554adb8ce1242f6e7b8051c5a0221df649b6
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-04 19:05:12 +0300

    Add

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -3,12 +3,11 @@ an empty class that we will implement later
 '''
 
 
-class BaseGeometry (object):
+class BaseGeometry:
     ''' there will be some implementations here
     # '''
-    def __dir__(self):
-        attributes =object.__dir__()
+    def __dir__(cls):
+        attributes = super().__dir__()
         return [x for x in attributes if x != '__init_subclass__']
   
 
-# print(dir(BaseGeometry))
