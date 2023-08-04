e6314934c37bf7dbeb3ccad199290da3a8206f79
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-04 11:13:09 +0300

    Add a function : checks object instance class

---
+++ python-inheritance/0-is_same_class.py
@@ -0,0 +1,8 @@
+'''
+    this is a function that returns true or false if an object is an exact instance of a class.
+'''
+
+def is_same_class(obj, a_class):
+    if isinstance(object, a_class):
+        return True
+    return False
