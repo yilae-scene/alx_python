12c7e6df0b76c571323798305ba54867f9a8597a
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-04 11:44:38 +0300

    Add a function

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,13 @@
+'''
+    the following function checks if the object is an instance of a class
+'''
+
+def inherits_from(obj, a_class):
+    ''' the function takes object and class 
+        checks whether the object is instance of class
+    '''
+    #obj = object
+    #a_class = class
+    if isinstance(obj, a_class):
+        return True
+    return False
