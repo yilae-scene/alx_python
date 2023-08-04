64c076d18385ec6e04ed9fb6b0d63e34c00c4878
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-04 14:56:28 +0300

    Modify the __dir__ to remove subclass attribute

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -4,6 +4,11 @@
 
 class BaseGeometry:
     ''' the class raises a an exception'''
+    def __dir__(self):
+        ''' remover the "__init_subclas__" in the subclass'''
+        attributes = super().__dir__()
+        return [x for x in attributes if x != '__init_subclass__']
+  
     def area(self):
         # raise an error
         raise Exception('area() is not implemented')
