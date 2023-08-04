9908d15a98920424a60894b84525a6b003d24236
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-04 16:58:40 +0300

    Add a sub-class from 5-base class

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,21 @@
+'''
+this is a class that inherits form the base_geomety class
+'''
+# import a file with number and put it in variable called BaseGeometry
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+
+    # check for value validation
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height     
+
+r = Rectangle(3, 5)
+
+print(r)
+print(dir(r))
