5cac14c1d101f74975d572db38689d078937f406
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-05 13:54:53 +0300

    Modify dir()

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,5 +1,7 @@
+''' 
+ meta class that will provide the dir method to BaseGeometry class
+'''
 
-# a meta class that will provide the dir method to BaseGeometry class
 
 class TopMeta(type):
     ''' 
@@ -7,6 +9,7 @@ class TopMeta(type):
     '''
 
     def __dir__(self):
+        # use an overloading to remove the 'initsubclass'
         attributes = super().__dir__()
         return [x for x in attributes if x != '__init_subclass__']
 
@@ -31,5 +34,3 @@ class BaseGeometry (metaclass=TopMeta):
             raise TypeError('{} must be an integer'.format(self.name))
         if value <= 0:
             raise ValueError('{} must be greater than 0'.format(self.name))
-
-
