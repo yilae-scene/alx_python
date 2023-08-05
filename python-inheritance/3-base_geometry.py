c2ed55feb5d8752cee133204d5e5684f5b87164f
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-05 13:25:43 +0300

    Modify so that it overides dir()

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,13 +1,27 @@
 ''' 
 an empty class that we will implement later
 '''
+# a meta class that will provide the dir method to BaseGeometry class
 
 
-class BaseGeometry:
-    ''' there will be some implementations here
-    # '''
-    def __dir__(cls):
+class TopMeta(type):
+    ''' 
+        remove the init__subclass so that BaseGeomety will be without it
+    '''
+
+    def __dir__(self):
         attributes = super().__dir__()
         return [x for x in attributes if x != '__init_subclass__']
-  
+
+
+class BaseGeometry (metaclass=TopMeta):
+    ''' 
+    there will be some implementations here
+    '''
+
+    def __dir__(self):
+        attributes = super().__dir__()
+        return [x for x in attributes if x != '__init_subclass__']
+
+
 
