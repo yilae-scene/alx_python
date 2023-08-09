82b2952969f461f28e1137ba79a6edaf40fdf1fa
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-09 21:12:54 +0300

    MOdify

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -9,10 +9,11 @@ Here we have a subclass of a Base class that will use a private methods as insta
 
 # from base import Base
 
+from models.base import Base
 
 ''' import Base from models.base'''
 
-from models.base import Base
+
 class Rectangle(Base):
     ''' define the class with private instances'''
 
