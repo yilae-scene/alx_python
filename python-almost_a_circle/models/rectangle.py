074c4e438ac20b7f58bbe50576861ef090216f38
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-09 22:33:51 +0300

    kfjkljgk

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -18,12 +18,11 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         # initalize the class
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        
+        super().__init__(id)
 
     @property
     # get the width by getter method
