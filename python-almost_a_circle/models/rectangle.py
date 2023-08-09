a58af3228c6d4ecb47d46b5c1a161870ebe5a1ca
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-09 22:18:07 +0300

    Modify

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -19,6 +19,10 @@ class Rectangle(Base):
     def __init__(self, width, height, x=0, y=0, id=None):
         # initalize the class
         super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         # private instances of the class.
         # check for width
         if type(width) is not int:
