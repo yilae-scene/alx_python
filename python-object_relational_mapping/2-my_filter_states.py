7ea155c9136528b0119431d94d033d2e262e69f6
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-22 19:25:15 +0300

    dlkfjkdjf

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+'''
+we start our projects by importing MySQLdb
+and using it to link to MySQL
+database we have created before.
+'''
+# import modules
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mydb = MySQLdb.connect(host = 'localhost' , user = sys.argv[1],passwd = sys.argv[2], port = 3306, db = sys.argv[3])
+
+    # create cursor
+    cur = mydb.cursor()
+    cur.execute("SELECT FROM states WHERE name == {} Order by states.id", format(sys.argv[4]))
+
+    # commit and close
+    mydb.commt()
+    cur.close()
