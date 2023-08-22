cffd53ce2715356460c88e1de81415b895225e95
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-22 19:39:20 +0300

    jdhfjhdf

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,16 +8,19 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    mydb = MySQLdb.connect(host = 'localhost' , user = sys.argv[1],passwd = sys.argv[2], port = 3306, db = sys.argv[3])
+    mydb = MySQLdb.connect(
+        host='localhost', user=sys.argv[1], passwd=sys.argv[2], port=3306, db=sys.argv[3])
 
     # create cursor
     cur = mydb.cursor()
-    cur.execute("SELECT FROM states WHERE name == {} Order by states.id", format(sys.argv[4]))
+    cur.execute(
+        "SELECT FROM states WHERE name == {} Order by states.id".format(sys.argv[4]))
 
     # fetch the results
     results = cur.fetchall()
     for result in results:
         print(result)
+
     # commit and close
     mydb.commt()
     cur.close()
