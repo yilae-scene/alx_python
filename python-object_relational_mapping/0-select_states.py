ed36981db17e0db34e3134486b27a18946607107
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-22 17:29:28 +0300

    Modify to accept CLI arguments

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,23 +2,28 @@
  we start our projects by importing MySQLdb and using it to link to MySQL database we have created before. 
 '''
 # import Mysqldb
+# import sys
 import MySQLdb
-
+import sys
 """
 make sure your code works on main page of the python module
 """
 if "__name__" == "__main__":
     # create connection to database using mysqldb
 
-    mydb = MySQLdb.connect(host = 'localhost', user = "root", passwd ='MaRcan2011yilikal', port = 3306, db = 'hbtn_0e_0_usa')
+    mydb = MySQLdb.connect(host='localhost', user=sys.argv[1],
+                           passwd=sys.argv[2], port=3306, db=sys.argv[3])
 
     # create cursor
     cur = mydb.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states ORDER BY states.id")
 
     # fetch all the selected states
     results = cur.fetchall()
-    
-    #print all states
+
+    # print all states
     for result in results:
         print(result)
+
+    # make sure it is stated in the mysql database
+    db.commit()
