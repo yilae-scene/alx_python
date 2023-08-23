e0d9d7103a987fec2b49dffe31c24e4af4df0025
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-23 12:23:08 +0300

    kjhjhg

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -23,5 +23,5 @@ if __name__ == "__main__":
         for result in results:
             print(result)
         # commit and close
-        mydbdb.commit()
+        mydb.commit()
         cur.close()
