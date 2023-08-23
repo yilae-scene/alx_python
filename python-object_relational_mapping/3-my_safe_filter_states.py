f080ded820330cf704f8d3b43546d6f22ed9c8a6
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-23 12:31:18 +0300

    Prevent SQL injection

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,8 @@
 '''
-this is a module that tries to avoid a user input SQL injection
-by trying to filter keywords that
-might be used by user that will harm the SQL databases.
+this is a module that tries to avoid 
+a user input SQL injection by trying 
+to filter keywords that might be used
+by user that will harm the SQL databases.
 '''
 # import modules
 import MySQLdb
@@ -9,14 +10,23 @@ import sys
 
 if __name__ == "__main__":
 
-    mydb = MySQLdb.connect(host = 'localhost', user = sys.argv[1] , passwd = sys.argv[2] ,port = 3306,  db = sys.argv[3])
-    
+    mydb = MySQLdb.connect(
+        host='localhost', user=sys.argv[1],
+        passwd=sys.argv[2], port=3306,  db=sys.argv[3])
+
     cur = mydb.cursor()
 
-    if 'TRUNCATE' in sys.argv[4] or 'DROP' in sys.argv[4] or 'SELECT' in sys.argv[4] or 'UPDATE' in sys.argv[4] or 'DELETE' in sys.argv[4]:
+    if 'TRUNCATE' in sys.argv[4] \
+            or 'DROP' in sys.argv[4] \
+            or 'SELECT' in sys.argv[4] \
+            or 'UPDATE' in sys.argv[4] \
+            or 'DELETE' in sys.argv[4]:
         print("use only one value")
     else:
-        cur.execute("SELECT id, name FROM states WHERE name = '{}'".format(sys.argv[4]))
+        cur.execute(
+            "SELECT id, name \
+            FROM states \
+            WHERE name = '{}'".format(sys.argv[4]))
 
         # print results
         results = cur.fetchall()
