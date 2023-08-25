7986ede055347aa6f56043660eb8aaba0275b543
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-25 15:25:17 +0300

    Documentation for class

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,37 +1,34 @@
 
 # import modules
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy import Integer, Column, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, Column, String
 
 """
 here we connect we import modules
 sqlalchemy to creat class used
 to create table.
 """
+
 base = declarative_base()
 
-if __name__ == "__main__":
-    """make sure that your class doesn't work \
-    if not imported """
 
-    class State(base):
-        """ a class that is used to create a table using 
-        sqlalchemy
-        """
-        """ a class that is used to create a table using 
-        sqlalchemy
-        Attributes:
+class State(base):
+    """ a class that is used to create a table using 
+    sqlalchemy
+    """
+    """ a class that is used to create a table using 
+    sqlalchemy
+    Attributes:
 
          id == that will auto_increment
          name == that used for naming.
-        """
-        
-        __table__ = "state"
-        # create columns
+    """
 
-        id = Column(Integer, unique=True, autoincrement=True,
-                    primary_key=True, nullable=False)
-        name = Column(String(128), nullable=False)
+    __table__ = "state"
+    # create columns
+    id = Column(Integer, unique=True, autoincrement=True,
+                primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
 
-        def __int__(self, name):
-            self.name = name
+    def __int__(self, name):
+        self.name = name
