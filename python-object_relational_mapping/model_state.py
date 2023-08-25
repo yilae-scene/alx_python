0a3fccd01124c5ef60c3cd80b9fbbd128d9fe67a
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-25 14:58:08 +0300

    Create class using sqlalchemy

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+"""
+here we connect we import modules
+sqlalchemy to creat class used
+to create table.
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, Column, String
+
+base = declarative_base()
+
+if __name__ == "__main__":
+    
+    class State(base):
+        id = Column(Integer, unique=True, autoincrement=True, primary_key=True, nullable= False)
+        name = Column (String(128), nullable= False)
+
+        def __int__(self, name):
+            self.name = name
