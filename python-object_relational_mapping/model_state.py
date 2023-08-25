c691c059ae52dfe21a5e8ab927b51380266a4e09
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-25 16:59:23 +0300

    jshdfjh

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,23 +1,42 @@
+# """
+# here we connect we import modules sqlalchemy to creat class used to create table.
+# """
+# # import modules
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy import Integer, Column, String
+
+# base = declarative_base()
+
+
+# class State(base):
+#     """ a class that is used to create a table using 
+#     sqlalchemy:-
+#      with id : INT
+#      and name : String attribues
+#     """
+#     __tablename__ = "states"
+#     id = Column(Integer, unique=True, autoincrement=True,
+#                 primary_key=True, nullable=False)
+#     name = Column(String(128), nullable=False)
+
+#     def __init__(self, name):
+#         self.name = name
+
 """
-here we connect we import modules sqlalchemy to creat class used to create table.
+state class and Base, an instance of declarative_base()
 """
-# import modules
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Integer, Column, String
+from sqlalchemy import Column, Integer, String
 
-base = declarative_base()
+Base = declarative_base()
 
 
-class State(base):
-    """ a class that is used to create a table using 
-    sqlalchemy:-
-     with id : INT
-     and name : String attribues
+class State(Base):
     """
-    __tablename__ = "states"
-    id = Column(Integer, unique=True, autoincrement=True,
-                primary_key=True, nullable=False)
+    contains the class definition of a State and an instance
+    Base = declarative_base() with id and name attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, unique=True,
+                auto_increment=True, nullable=False)
     name = Column(String(128), nullable=False)
-
-    def __init__(self, name):
-        self.name = name
