b8d6877eaca43e616a888cc5d5df25331bb93c29
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-26 19:28:20 +0300

    List all states

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,22 @@
+"""
+create a script that lists all for states using
+sqlalchemy.
+"""
+# import modules
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+user = sys.argv[1]
+passwd = sys.argv[2]
+db = sys.argv[3]
+if __name__ == "__main__":
+
+    path = ("Mysql+Mysqldb://{}:{}@loclhost:3306/{}".format(user, passwd, db))
+
+    mydb = creating_engine(path)
+    session = sessionmaker(bind=mydb)
+    new_session = session()
+
+    for instance in session.query(State).order_by(State.id):
+        print("{}:{}".format(state.id, state.name))
