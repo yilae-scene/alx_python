ac7df62ab70f74af336d2ed0ffdd003a10746823
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-26 20:36:10 +0300

    jjjj

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,10 +8,11 @@ from sqlalchemy import create_engine
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 
-user = sys.argv[1]
-passwd = sys.argv[2]
-db = sys.argv[3]
+
 if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
 
     path = ("Mysql+Mysqldb://{}:{}@loclhost:3306/{}".format(user, passwd, db))
 
