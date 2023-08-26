d29a3a221f083872f8957772bb2d446a649d278a
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-08-26 21:35:55 +0300

    Print First

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+"""
+create a script that lists all for states using
+sqlalchemy.
+"""
+# import moduless
+import sys
+from sqlalchemy import create_engine
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+# variables
+user = sys.argv[1]
+passwd = sys.argv[2]
+db = sys.argv[3]
+if __name__ == "__main__":
+
+    mydb = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(user, passwd, db))
+    Session = sessionmaker(bind=mydb)
+    new_session = Session()
+
+    for instance in new_session.query(State).first():
+        print("{:d}: {}".format(instance.id, instance.name))
