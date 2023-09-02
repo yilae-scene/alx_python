654d1e26dc2875f220466dbdb6f6253a0b24b04b
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-09-02 17:59:19 +0300

    Add  Odd_Even in jinja2

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -52,7 +52,7 @@ def number_template(n):
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_even_odd(n):
-    return render_template("5-number.html", number = n)
+    return render_template("6-number.html", number = n)
 
 
 if __name__ == "__main__":
