e30685ed58f476f18be731e3014675617a664f15
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-09-02 20:50:54 +0300

    Odd_Even

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -52,6 +52,10 @@ def number_template(n):
 
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_even_odd(n):
+    if n % 2 == 0:
+        result = "even"
+    else:
+        result = "odd"
     return render_template("6-number_odd_even.html", number = n)
 
 
