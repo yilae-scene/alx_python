b6397531d5ad2c7049ea55f93107920a0e7bfb5f
Yilikal Ayalew <yilikalayalew76@gmail.com>
2023-09-02 21:14:08 +0300

    Commit

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -54,11 +54,10 @@ def number_template(n):
 @app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
 def number_even_odd(n):
     if n % 2 == 0:
-        result = "even"
+        result = 'even'
     else:
-        result = "odd"
-    return render_template("6-number_odd_even.html", number = n, result = result)
-
+        result = 'odd'
+        return render_template('6-number_odd_or_even.html', n = n, result=result)
 
 if __name__ == "__main__":
     # web application must be listening on 0.0.0.0, port 5000
